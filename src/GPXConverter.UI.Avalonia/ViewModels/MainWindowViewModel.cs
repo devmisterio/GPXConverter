81659d224efa9ffd6b5937a708ec4694e3a2b392
Semih Arslan <arslan_semih@yahoo.com>
2025-03-29 10:32:24 +0300

    UI init

---
+++ src/GPXConverter.UI.Avalonia/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace GPXConverter.UI.Avalonia.ViewModels;
+
+public partial class MainWindowViewModel : ViewModelBase
+{
+    public string Greeting { get; } = "Welcome to Avalonia!";
+}
---
+++ src/GPXConverter.UI.Avalonia/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace GPXConverter.UI.Avalonia.ViewModels;
+
+public class ViewModelBase : ObservableObject
+{
+}
